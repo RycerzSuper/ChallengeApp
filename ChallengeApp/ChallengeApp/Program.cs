96c39e5376a88452c3cd9737e8e4865fb7b3e286
adrian <adrian@KOMPUTER_AJ>
2024-03-20 19:52:40 +0000

    Zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Adrian";
+var isMale = true;
+var age = 15;
+
+if (isMale == false && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Eva" && age == 30)
+{
+    Console.WriteLine("Eva, 30 lat");
+}
+else if (isMale == true && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else
+{
+    
+}
